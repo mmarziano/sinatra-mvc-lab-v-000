1db7c711b43ca692167c4882d91cd886babdc188
Monica Marziano <mmarziano1@gmail.com>
2019-04-14 18:15:05 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,27 @@
+class PigLatinizer 
+  attr_accessor :user_phrase, :phrase
+  
+  def initialize(user_phrase)
+    @user_phrase = user_phrase
+    self.piglatinize
+  end 
+  
+  def piglatinize(user_phrase)
+    words = []
+    piglatin_words = []
+    words << self.user_phrase.split(' ')
+    words.first.each do |i|
+      if i.downcase.start_with?('a','e','i','o','u')
+        new_word = i + 'way'
+        piglatin_words << new_word
+      else
+        modified = i.slice(1, i.split('').count)
+        first_letter = i.split('').first
+        new_word = modified + first_letter + 'ay'
+        piglatin_words << new_word
+      end 
+      @phrase = piglatin_words.join(' ')
+    end
+  end 
+  
+end 
