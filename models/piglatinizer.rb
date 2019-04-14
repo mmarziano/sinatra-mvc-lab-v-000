addfd4acd5bffa6b67be932e90a606de933f03ea
Monica Marziano <mmarziano1@gmail.com>
2019-04-14 19:15:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,17 +6,18 @@ class PigLatinizer
     piglatin_words = []
     words << user_phrase.split(' ')
     words.first.each do |i|
-      if i.downcase.start_with?('a','e','i','o','u')
-        new_word = i + 'way'
-        piglatin_words << new_word
-      else
-          if i.downcase.split(/([aeiou].*)/).first.length > 1 
+      if i.downcase.split(/([aeiou].*)/).first.length > 1 
             cluster = i.downcase.split(/([aeiou].*)/).first.length
-            modified = i.slice(cluster-1, i.split('').count)
-            first_letters = i.split('')[0-cluster-1]
-            new_word = modified + first_letters + 'ay'
-            piglating_words << new_word
-          end 
+            modified = i.slice(cluster, i.split('').count - 1)
+            first_consonants = i.split('')[0,cluster-1]
+            new_word = modified + first_consonants + 'ay'
+            piglatin_words << new_word
+      elsif
+        if i.downcase.start_with?('a','e','i','o','u')
+          new_word = i + 'way'
+          piglatin_words << new_word
+        end
+      else
         modified = i.slice(1, i.split('').count)
         first_letter = i.split('').first
         new_word = modified + first_letter + 'ay'
