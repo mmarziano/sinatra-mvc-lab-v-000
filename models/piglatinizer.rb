da3593066f8551d59fde7b7e83fb618bbc8cc4d1
Monica Marziano <mmarziano1@gmail.com>
2019-04-14 19:03:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,8 +10,13 @@ class PigLatinizer
         new_word = i + 'way'
         piglatin_words << new_word
       else
-        i.downcase.split(/([aeiou].*)/).first.length
-        binding.pry
+          if i.downcase.split(/([aeiou].*)/).first.length > 1 
+            cluster = i.downcase.split(/([aeiou].*)/).first.length
+            modified = i.slice(cluster-1, i.split('').count)
+            first_letters = i.split('')[0-cluster-1]
+            new_word = modified + first_letters + 'ay'
+            piglating_words << new_word
+          end 
         modified = i.slice(1, i.split('').count)
         first_letter = i.split('').first
         new_word = modified + first_letter + 'ay'
